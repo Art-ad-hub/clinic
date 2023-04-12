54dd124298f12d7e17c65c3f589dd2401d4aed96
Artem <130403999+Art-ad-hub@users.noreply.github.com>
2023-04-12 22:34:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInQueue;
+            int waitingTimeOnePerson = 10;
+            int hour;
+            int minute;
+            int minuteInHour = 60;
+
+            Console.Write("Сколько человек перед вами в очереди: ");
+            peopleInQueue = Convert.ToInt32(Console.ReadLine());
+            minute = peopleInQueue * waitingTimeOnePerson;
+            hour = minute / minuteInHour;
+            minute = minute % minuteInHour;
+            Console.WriteLine($"Вам придется подождать в очереди {hour}ч.{minute}минут.");
+        }
+    }
+}
